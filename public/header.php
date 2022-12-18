34e8414bc86b12bd41218b172b8a9d9724a583dd
易航网络 <xh118039@qq.com>
2022-12-19 00:32:14 +0800

    1111

--- public/header.php
+++ public/header.php
@@ -170,7 +170,7 @@
 					<?php if ($category->levels === 0) : ?>
 						<?php $children = $category->getAllChildren($category->mid); ?>
 						<?php if (empty($children)) : ?>
-							<a class="item <?php echo $this->is('category', $category->slug) ? 'active' : '' ?>" href="<?php $category->permalink(); ?>" title="<?php $category->name(); ?>"><?php $category->name(); ?></a>
+							<a class="item <?php echo $this->is('category', $category->slug) ? 'active' : '' ?>" href="<?php $category->permalink(); ?>" title="<?= htmlentities($category->name) ?>"><?php $category->name(); ?></a>
 						<?php else : ?>
 							<div class="joe_dropdown" trigger="hover">
 								<div class="joe_dropdown__link">
@@ -376,12 +376,12 @@
 							<?php $children = $category->getAllChildren($category->mid); ?>
 							<?php if (empty($children)) : ?>
 								<li>
-									<a class="link <?php echo $this->is('category', $category->slug) ? 'current' : '' ?>" href="<?php $category->permalink(); ?>" title="<?php $category->name(); ?>"><?php $category->name(); ?></a>
+									<a class="link <?php echo $this->is('category', $category->slug) ? 'current' : '' ?>" href="<?php $category->permalink(); ?>" title="<?= htmlentities($category->name) ?>"><?php $category->name(); ?></a>
 								</li>
 							<?php else : ?>
 								<li>
 									<div class="link panel <?php echo $this->is('category', $category->slug) ? 'current' : '' ?>">
-										<a href="<?php $category->permalink(); ?>" title="<?php $category->name(); ?>"><?php $category->name(); ?></a>
+										<a href="<?php $category->permalink(); ?>" title="<?= htmlentities($category->name)  ?>"><?php $category->name(); ?></a>
 										<svg class="icon" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" width="13" height="13">
 											<path d="M624.865 512.247L332.71 220.088c-12.28-12.27-12.28-32.186 0-44.457 12.27-12.28 32.186-12.28 44.457 0l314.388 314.388c12.28 12.27 12.28 32.186 0 44.457L377.167 848.863c-6.136 6.14-14.183 9.211-22.228 9.211s-16.092-3.071-22.228-9.211c-12.28-12.27-12.28-32.186 0-44.457l292.155-292.16z" />
 										</svg>

4d17b41f01696e07735f70817f32341c5e418224
易航 <2136118039@qq.com>
2024-08-13 02:48:00 +0800

    友情链接新增rel属性

--- options/friend.php
+++ options/friend.php
@@ -5,11 +5,11 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 $JFriends = new Typecho_Widget_Helper_Form_Element_Textarea(
 	'JFriends',
 	NULL,
-	'易航博客 || http://blog.bri6.cn || http://blog.bri6.cn/favicon.ico || 一名编程爱好者的博客，记录与分享编程、学习中的知识点',
+	'易航博客 || http://blog.bri6.cn || http://blog.bri6.cn/favicon.ico || 一名编程爱好者的博客，记录与分享编程、学习中的知识点 || friend',
 	'友情链接（非必填）',
 	'介绍：用于填写友情链接 <br />
          注意：您需要先增加友链链接页面（新增独立页面-右侧模板选择友链），该项才会生效 <br />
-         格式：博客名称 || 博客地址 || 博客头像 || 博客简介 <br />
+         格式：博客名称 || 博客地址 || 博客头像 || 博客简介 || 链接rel属性<br />
          其他：一行一个，一行代表一个友链'
 );
 $JFriends->setAttribute('class', 'joe_content joe_friend');

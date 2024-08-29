1cd61b155ad30fb6c6efb7aa5668a574c4e58ed6
易航 <2136118039@qq.com>
2024-08-29 11:46:07 +0800

    update

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ if (!defined('__TYPECHO_ROOT_DIR__')) {
 	exit;
 }
 
-define('JOE_VERSION', '1.3191');
+define('JOE_VERSION', '1.3192');
 define('JOE_ROOT', dirname(__FILE__) . '/');
 /* Joe核心文件 */
 require_once(__DIR__ . '/public/common.php');

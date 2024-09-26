2eb5abec4fa302eff46de0dcafc23513f3ae230a
易航 <2136118039@qq.com>
2024-09-27 00:32:04 +0800

    update

--- assets/typecho/write/dist/index.bundle.js
+++ assets/typecho/write/dist/index.bundle.js
@@ -17295,7 +17295,7 @@
 						const i = t[e];
 						n += `<div class="tabbar-item ${"经典" === e ? "active" : ""}" data-show="${e}">${e}</div>`,
 							r +=
-							`<div class="lists ${"经典" === e ? "active" : ""}" data-show="${e}">${i.map((t => `<div class="lists-item" data-text="${t.data}">${"颜文字" === e ? t.icon : `<img src="${window.JoeConfig.themeURL + t.icon}">`}</div>`)).join(" ")}</div>`
+							`<div class="lists ${"经典" === e ? "active" : ""}" data-show="${e}">${i.map((t => `<div class="lists-item" data-text="${t.data}" title="${item.data}">${"颜文字" === e ? t.icon : `<img src="${window.JoeConfig.themeURL + t.icon}">`}</div>`)).join(" ")}</div>`
 					}
 					this._openModal({
 						title: "普通表情",

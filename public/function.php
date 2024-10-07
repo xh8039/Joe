12d686b6fa4936ea19ed99a04dfc85911cd83467
易航 <2136118039@qq.com>
2024-10-07 21:14:47 +0800

    update

--- public/function.php
+++ public/function.php
@@ -303,16 +303,16 @@ function url_builder($url, array $param)
 function markdown_filter($text)
 {
 	// 居中标题标签
-	$text = preg_replace('/{mtitle title="(.*?)"\/}/', '$1', $text);
+	$text = preg_replace('/{mtitle title="(.*?)"\/}/', ' $1', $text);
  
 	// 云盘下载
-	$text = preg_replace('/{cloud title="(.*?)" type="\w+" url="(.*?)" password="(.*?)"\/}/', '$1 下载地址：$2，提取码：$3', $text);
+	$text = preg_replace('/{cloud title="(.*?)" type="\w+" url="(.*?)" password="(.*?)"\/}/', ' $1 下载地址：$2，提取码：$3', $text);
 
 	// 音乐标签
-	$text = preg_replace('/{mp3 name="(.*?)" artist="(.*?)"/', '$1 - $2', $text);
+	$text = preg_replace('/{mp3 name="(.*?)" artist="(.*?)"/', ' $1 - $2', $text);
 
 	// 标签中有content值
-	$text = preg_replace('/{.*?content="(.*?)"\/}/', '$1', $text);
+	$text = preg_replace('/{.*?content="(.*?)"\/}/', ' $1', $text);
 
 	// 标签页
 	$text = preg_replace('/{tabs}(.*?){\/tabs}/is', '$1', $text);
@@ -324,20 +324,22 @@ function markdown_filter($text)
 	$text = preg_replace('/{timeline}(.*?){\/timeline}/is', '$1', $text);
 
 	// 描述卡片
-	$text = preg_replace('/{card-describe title="(.*?)"}(.*?){\/card-describe}/', '$1 - $2', $text);
+	$text = preg_replace('/{card-describe title="(.*?)"}(.*?){\/card-describe}/', ' $1 - $2', $text);
 
 	// 折叠面板
 	$text = preg_replace('/{collapse}(.*?){\/collapse}/is', '$1', $text);
-	$text = preg_replace('/{collapse-item label="(.*?)" open}(.*?){\/collapse-item}/', '$1 - $2', $text);
+	$text = preg_replace('/{collapse-item label="(.*?)" open}(.*?){\/collapse-item}/', ' $1 - $2', $text);
 
 	// 宫格
 	$text = preg_replace('/{gird column="\d+" gap="\d+"}(.*?){\/gird}/is', '$1', $text);
 
 	// 其他开合标签
-	$text = preg_replace('/{\w+.*?}(.*?){\/\w+}/is', '$1', $text);
+	$text = preg_replace('/{\w+.*?}(.*?){\/\w+}/is', ' $1', $text);
 
 	// 剩下没有文本的单标签
 	$text = preg_replace('/{.*?\/}/', '', $text);
+
+	$text = trim($text);
 	return $text;
 }
 

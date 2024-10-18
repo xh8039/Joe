a2dd54b932bc01560329e0662457d05d79872fa5
易航 <2136118039@qq.com>
2024-10-18 22:11:41 +0800

    update

--- module/player.php
+++ module/player.php
@@ -8,13 +8,13 @@ require_once dirname(dirname(dirname(dirname(__DIR__)))) . DIRECTORY_SEPARATOR .
 \Widget\Init::alloc();
 require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'public' . DIRECTORY_SEPARATOR . 'function.php';
 if (!empty(Helper::options()->JCustomPlayer)) exit('已使用第三方视频播放器！');
-$db = Typecho_Db::get();
-$row = $db->fetchRow($db->select('text,title')->from('table.contents')->where('cid = ?', $cid));
-if (sizeof($row) > 0) {
-	if (!stripos($row['text'], $_GET['url'])) exit('视频不存在！');
-} else {
-	exit('视频不存在！');
-}
+// $db = Typecho_Db::get();
+// $row = $db->fetchRow($db->select('text,title')->from('table.contents')->where('cid = ?', $cid));
+// if (sizeof($row) > 0) {
+// 	if (!stripos($row['text'], $_GET['url'])) exit('视频不存在！');
+// } else {
+// 	exit('视频不存在！');
+// }
 
 $autoplay = $_GET['autoplay'] ? 'true' : 'false';
 $theme = $_GET['theme'] ? $_GET['theme'] : '#409eff';
@@ -90,24 +90,43 @@ $MSE = empty($MSE) ? null : ('<script src="' . $MSE . '"></script>' . PHP_EOL);
 	<?= $MSE ?>
 	<script src="<?= joe\cdn('dplayer/1.27.0/DPlayer.min.js') ?>"></script>
 	<script>
-		window.videoPlayer = new DPlayer({
-			container: document.getElementById('dplayer'), // 播放器容器元素
-			autoplay: <?= $autoplay ?>, // 视频自动播放
-			theme: '<?= $theme ?>', // 主题色
-			lang: 'zh-cn', // 可选值: 'en', 'zh-cn', 'zh-tw'
-			preload: '<?= $preload ?>', // 视频预加载，可选值: 'none', 'metadata', 'auto'
-			loop: <?= $loop ?>, // 视频循环播放
-			screenshot: <?= $screenshot ?>, // 开启截图，如果开启，视频和视频封面需要允许跨域
-			airplay: true, // 在 Safari 中开启 AirPlay
-			volume: 1, // 默认音量，请注意播放器会记忆用户设置，用户手动设置音量后默认音量即失效
-			playbackSpeed: [2.00, 1.75, 1.50, 1.25, 1.00, 0.75, 0.50, 0.25], // 可选的播放速率，可以设置成自定义的数组
-			video: {
-				url: "<?= $url ?>",
-				pic: <?= $pic ?>,
-				type: <?= $video_type ?>
-			},
-			customType: <?= $customType ?>
-		});
+		window.onload = () => {
+			// 检查是否在 iframe 中
+			if (window.parent !== window) {
+				// 获取父页面 URL
+				const parentUrl = window.parent.location.origin;
+
+				// 获取当前页面 URL
+				const currentUrl = window.location.origin;
+
+				// 比较 URL 是否相同
+				if (parentUrl !== currentUrl) {
+					alert('禁止不同源的 iframe 访问！');
+					return false; // 被父页面包括但不同源
+				}
+			} else {
+				alert('禁止直接访问！');
+				return false; // 没有被父页面包括
+			}
+			window.videoPlayer = new DPlayer({
+				container: document.getElementById('dplayer'), // 播放器容器元素
+				autoplay: <?= $autoplay ?>, // 视频自动播放
+				theme: '<?= $theme ?>', // 主题色
+				lang: 'zh-cn', // 可选值: 'en', 'zh-cn', 'zh-tw'
+				preload: '<?= $preload ?>', // 视频预加载，可选值: 'none', 'metadata', 'auto'
+				loop: <?= $loop ?>, // 视频循环播放
+				screenshot: <?= $screenshot ?>, // 开启截图，如果开启，视频和视频封面需要允许跨域
+				airplay: true, // 在 Safari 中开启 AirPlay
+				volume: 1, // 默认音量，请注意播放器会记忆用户设置，用户手动设置音量后默认音量即失效
+				playbackSpeed: [2.00, 1.75, 1.50, 1.25, 1.00, 0.75, 0.50, 0.25], // 可选的播放速率，可以设置成自定义的数组
+				video: {
+					url: "<?= $url ?>",
+					pic: <?= $pic ?>,
+					type: <?= $video_type ?>
+				},
+				customType: <?= $customType ?>
+			});
+		}
 	</script>
 </body>
 

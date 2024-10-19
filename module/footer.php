3b81308c892af901b09b2a819299022e4cb53b50
易航 <2136118039@qq.com>
2024-10-19 12:39:50 +0800

    update

--- module/footer.php
+++ module/footer.php
@@ -92,42 +92,9 @@ if ($this->options->JMusic == 'on') {
 </div>
 
 <?php
-if (!empty($this->options->JFooterTabbar) && joe\isMobile()) {
-	$footer_tabbar = joe\optionMulti($this->options->JFooterTabbar);
-	if (!empty($footer_tabbar)) {
-?>
-		<link rel="stylesheet" href="<?= joe\theme_url('assets/css/options/footer-tabbar.css') ?>">
-		<div class="footer-tabbar">
-			<?php
-			foreach ($footer_tabbar as $value) {
-			?>
-				<a class="tabbar-item" title="<?= $value[0] ?? '' ?>" href="<?= $value[1] ?? '' ?>" target="<?= $value[2] ?? '' ?>">
-					<icon <?= empty($value[4]) ? '' : 'style="font-size:' . $value[4] . ';"' ?>>
-						<svg class="icon svg" aria-hidden="true">
-							<use xlink:href="<?= $value[3] ?? '' ?>"></use>
-						</svg>
-					</icon>
-					<text><?= $value[0] ?? '' ?></text>
-				</a>
-			<?php
-			}
-			?>
-		</div>
-		<script>
-			const element = document.querySelector('.footer-tabbar');
-			const height = element.clientHeight;
-			document.getElementById('cc-myssl-seal').style.bottom = (height - 1) + 'px';
-			document.querySelector('body').style.paddingBottom = (height - 1) + 'px';
-			document.querySelector('.aplayer.aplayer-fixed .aplayer-body').style.bottom = (height - 1) + 'px';
-		</script>
-	<?php
-	}
-}
-
-
 // SSL安全认证
 if ($this->options->JPendant_SSL == 'on') {
-	?>
+?>
 	<style>
 		html .joe_action {
 			bottom: <?= empty($footer_tabbar) ? '80px' : '150px' ?>;
@@ -148,7 +115,39 @@ if ($this->options->JPendant_SSL == 'on') {
 			<img src="<?= Joe\theme_url('assets/images/myssl-id.png') ?>" alt="SSL" style="width: 100%; height: 100%"></a>
 		</div>
 	</div>
+	<?php
+}
+
+if (!empty($this->options->JFooterTabbar) && joe\isMobile()) {
+	$footer_tabbar = joe\optionMulti($this->options->JFooterTabbar);
+	if (!empty($footer_tabbar)) {
+	?>
+		<link rel="stylesheet" href="<?= joe\theme_url('assets/css/options/footer-tabbar.css') ?>">
+		<div class="footer-tabbar">
+			<?php
+			foreach ($footer_tabbar as $value) {
+			?>
+				<a class="tabbar-item" title="<?= $value[0] ?? '' ?>" href="<?= $value[1] ?? '' ?>" target="<?= $value[2] ?? '' ?>">
+					<icon <?= empty($value[4]) ? '' : 'style="font-size:' . $value[4] . ';"' ?>>
+						<svg class="icon svg" aria-hidden="true">
+							<use xlink:href="<?= $value[3] ?? '' ?>"></use>
+						</svg>
+					</icon>
+					<text><?= $value[0] ?? '' ?></text>
+				</a>
+			<?php
+			}
+			?>
+		</div>
+		<script>
+			const element = document.querySelector('.footer-tabbar');
+			const height = element.clientHeight - 1;
+			if (document.getElementById('cc-myssl-seal')) document.getElementById('cc-myssl-seal').style.bottom = height + 'px';
+			if (document.querySelector('.aplayer.aplayer-fixed .aplayer-body')) document.querySelector('.aplayer.aplayer-fixed .aplayer-body').style.bottom = height + 'px';
+			document.querySelector('body').style.paddingBottom = height + 'px';
+		</script>
 <?php
+	}
 }
 ?>
 

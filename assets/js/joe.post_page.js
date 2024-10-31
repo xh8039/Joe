fa2a65ea6320c7207fdf003f342d609dda153ddb
易航 <2136118039@qq.com>
2024-10-31 21:38:00 +0800

    update

--- assets/js/joe.post_page.js
+++ assets/js/joe.post_page.js
@@ -278,13 +278,13 @@ document.addEventListener('DOMContentLoaded', () => {
 					console.log(videoPlayer.video.paused);
 					videoPlayer.video.play();
 				}
+				videoPlayer.video.addEventListener('error', () => {
+					setTimeout(() => {
+						console.log('视频加载错误：' + videoPlayer.video.src);
+						next();
+					}, 2000);
+				})
 			});
-			videoPlayer.video.addEventListener('error', () => {
-				setTimeout(() => {
-					console.log('视频加载错误：' + videoPlayer.video.src);
-					next();
-				}, 2000);
-			})
 		}
 	}
 

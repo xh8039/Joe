0da3107a6a18b4516f5a8c3d6b0a76de02043e25
易航 <2136118039@qq.com>
2024-10-31 21:09:41 +0800

    update

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ if (!defined('__TYPECHO_ROOT_DIR__')) {
 	exit;
 }
 
-define('JOE_VERSION', '1.3284');
+define('JOE_VERSION', '1.3285');
 define('JOE_ROOT', dirname(__FILE__) . '/');
 define('THEME_NAME', basename(__DIR__));
 define('JOE_BASE_API', Helper::options()->rewrite == 0 ? Helper::options()->rootUrl . '/index.php/joe/api' : Helper::options()->rootUrl . '/joe/api');

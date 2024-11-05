d67b14c1bda9b377f6393be83c07534eaf9298b3
易航 <2136118039@qq.com>
2024-11-05 10:33:57 +0800

    update

--- module/single/comment.php
+++ module/single/comment.php
@@ -4,7 +4,7 @@ if (!defined('__TYPECHO_ROOT_DIR__')) {
 	exit;
 }
 $this->comments()->to($comments);
-$is_comment = ($this->allow('comment') && $this->options->JCommentStatus != "off" && ($this->options->JcommentLogin != 'on' || !is_numeric(USER_ID))) ? true : false;
+$is_comment = ($this->allow('comment') && $this->options->JCommentStatus != "off" && ($this->options->JcommentLogin != 'on' || is_numeric(USER_ID))) ? true : false;
 ?>
 <div class="joe_comment__title title-theme">评论 <small><?= $is_comment ? (empty($this->commentsNum) ? '抢沙发' : '共' . $this->commentsNum . '条') : null ?></small></div>
 <div class="joe_comment" id="comment_module">

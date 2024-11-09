87e211c1426a28c929c2b5e7f5b2df0f5b1e528a
易航 <2136118039@qq.com>
2024-11-09 14:00:54 +0800

    update

--- functions.php
+++ functions.php
@@ -16,8 +16,8 @@ function themeConfig($form)
 	joe\install();
 ?>
 	<link rel="stylesheet" href="<?= joe\theme_url('assets/typecho/config/css/joe.config.css') ?>">
-	<script src="<?= joe\cdn('jquery/3.6.0/jquery.min.js') ?>"></script>
-	<script src="<?= joe\cdn('layer/3.5.1/layer.min.js') ?>"></script>
+	<script src="<?= joe\theme_url('assets/plugin/jquery/3.6.0/jquery.min.js') ?>"></script>
+	<script src="<?= joe\theme_url('assets/plugin/layer/3.5.1/layer.min.js') ?>"></script>
 	<script>
 		window.Joe = {
 			title: `<?= trim(Helper::options()->title ?? '') ?>`,

e642cfb82d46b18d05a62ae8c27e54af3a4f13ee
易航 <2136118039@qq.com>
2024-11-10 21:58:23 +0800

    update

--- module/post/adverts.php
+++ module/post/adverts.php
@@ -11,7 +11,7 @@ if (!empty($post_ad) && !joe\detectSpider() && $this->fields->global_advert != '
 		<?php
 		foreach ($post_ad as $advert) : ?>
 			<a class="joe_post__ad-link" <?= empty($advert[1]) ? '' : 'href="' . $advert[1] . '" target="_blank"' ?> rel="nofollow">
-				<img referrerpolicy="no-referrer" rel="noreferrer" width="100%" class="image lazyload" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" data-src="<?= $advert[0] ?>" alt="<?= empty($advert[2]) ? '' : $advert[2] ?>" title="<?= empty($advert[2]) ? '' : $advert[2] ?>" />
+				<img referrerpolicy="no-referrer" rel="noreferrer" width="100%" class="image lazyload" src="<?= joe\theme_url('assets/images/thumb/thumbnail-sm.svg', false) ?>" data-src="<?= $advert[0] ?>" alt="<?= empty($advert[2]) ? '' : $advert[2] ?>" title="<?= empty($advert[2]) ? '' : $advert[2] ?>" />
 				<?= empty($advert[2]) ? '' : '<span class="icon">' . $advert[2] . '</span>' ?>
 			</a>
 		<?php endforeach; ?>

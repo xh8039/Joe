57612dfe291a2af4fbbf4eacdcc146b3f58ffcf7
易航 <2136118039@qq.com>
2024-11-12 01:32:31 +0800

    update

--- public/route.php
+++ public/route.php
@@ -263,32 +263,80 @@ function _handleAgree($self)
 function _getRecord($self)
 {
 	$self->response->setStatus(200);
-	$client = new \network\http\Client;
-	$client->param([
-		'url' => $self->request->site
-	]);
-	$output = $client->get('https://api.fish9.cn/api/baidu/')->toArray();
-	if (is_array($output)) {
-		if (isset($output['baidu']) && $output['baidu']) {
-			$self->response->throwJson(array("data" => "已收录"));
+	$cid = $self->request->cid;
+	/* sql注入校验 */
+	if (!preg_match('/^\d+$/',  $cid)) {
+		return $self->response->throwJson(array("code" => 0, "data" => "非法请求！已屏蔽！"));
+	}
+	$baidu_index = baidu_index($self->request->site);
+	if (is_bool($baidu_index['index'])) {
+		if ($baidu_index['index']) {
+			$self->response->throwJson(["data" => "已收录", 'response' => $baidu_index['response']]);
 		} else {
-			$cid = $self->request->cid;
-			/* sql注入校验 */
-			if (!preg_match('/^\d+$/',  $cid)) {
-				return $self->response->throwJson(array("code" => 0, "data" => "非法请求！已屏蔽！"));
-			}
 			$db = Typecho_Db::get();
 			$sql = $db->select('str_value')->from('table.fields')->where('cid = ?', $cid)->where('name = ?', 'baidu_push');
 			$row = $db->fetchRow($sql);
 			if ($row && $row['str_value'] == 'yes') {
-				$self->response->throwJson(["data" => "未收录，已推送"]);
+				$self->response->throwJson(["data" => "未收录，已推送", 'response' => $baidu_index['response']]);
 			} else {
-				$self->response->throwJson(array("data" => "未收录"));
+				$self->response->throwJson(["data" => "未收录", 'response' => $baidu_index['response']]);
 			}
 		}
 	} else {
-		$self->response->throwJson(array("data" => "检测失败"));
+		$self->response->throwJson(["data" => "检测失败", 'index' => $baidu_index['index'], 'response' => $baidu_index['response']]);
 	}
+}
+
+function baidu_index($url)
+{
+	$index = false;
+	$url = preg_replace('/^https?:\/\//', '', $url);
+	$user_agents = [
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:71.0) Gecko/20100101 Firefox/71.0",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36",
+		"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36 *OPR*/44.0.2510.1218",
+		"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.57.2 (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2",
+		"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36",
+		"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36",
+		"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.108 Safari/537.36 *2345Explorer*/8.4.1.14855",
+		"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36 *JuziBrowser*",
+		"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.75 Safari/537.36 *LBBROWSER*",
+		"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.104 Safari/537.36 Core/1.53.2441.400 *QQBrowser*/9.5.10632.400",
+		"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.221 Safari/537.36 SE 2.X *MetaSr* 1.0",
+		"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) *Maxthon*/5.0.2.2000 Chrome/47.0.2526.73 Safari/537.36",
+		"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 *UBrowser*/6.1.2107.204 Safari/537.36",
+		"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 *BIDUBrowser*/8.4 Safari/537.36",
+		"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36 *TheWorld* 7",
+		"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 *YaBrowser*/17.3.1.840 *Yowser*/2.5 Safari/537.36",
+		"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.63 Safari/537.36 *Qiyu*/2.1.0.0",
+		"Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5",
+	];
+	$user_agent = $user_agents[array_rand($user_agents)];
+	$client = new \network\http\Client;
+	$client->param([
+		'wd' => $url,
+		'rn' => 1,
+		'tn' => 'json',
+		'ie' => 'utf-8',
+		'cl' => 3,
+		'f' => 9
+	]);
+	$client->header([
+		'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+		'Accept-Language' => 'zh-CN,zh;q=0.9',
+		'User-Agent' => $user_agent,
+		'Connection' => 'keep-alive',
+		'Host' => 'www.baidu.com'
+	]);
+	$response = $client->get('https://www.baidu.com/s')->toArray();
+	if (is_array($response) && !empty($response['feed']['entry'][0]['url'])) {
+		$baidu_url = preg_replace('/^https?:\/\//', '', $response['feed']['entry'][0]['url']);
+		if ($baidu_url == $url) $index = true;
+	}
+	return [
+		'index' => $index,
+		'response' => $response
+	];
 }
 
 /* 主动推送到百度收录 已测试 √ */

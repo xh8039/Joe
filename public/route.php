12ed52353a71642fab5484e6be63a90846a2e3df
易航 <2136118039@qq.com>
2024-11-12 02:01:28 +0800

    update

--- public/route.php
+++ public/route.php
@@ -300,7 +300,7 @@ function baidu_index($url)
 		'cl' => 3,
 		'f' => 9
 	]);
-	$cookie = empty($cookie) ? '' : trim(Helper::options()->Baidu_Index_Cookie);
+	$cookie = empty(Helper::options()->Baidu_Index_Cookie) ? '' : trim(Helper::options()->Baidu_Index_Cookie);
 	$client->header([
 		'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
 		'accept-encoding' => 'gzip, deflate, br, zstd',

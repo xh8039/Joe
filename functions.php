111b6010a478ced16646ece5eca72974e5e6d9d4
易航 <2136118039@qq.com>
2024-11-16 00:05:19 +0800

    V1.3299

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ if (!defined('__TYPECHO_ROOT_DIR__')) {
 	exit;
 }
 
-define('JOE_VERSION', '1.3298');
+define('JOE_VERSION', '1.3299');
 define('JOE_ROOT', dirname(__FILE__) . '/');
 
 /* Joe核心文件 */

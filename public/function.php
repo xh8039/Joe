8b1e4ae8cf55f16b854a77794f49e3073ba80dc4
易航 <2136118039@qq.com>
2024-11-17 10:08:25 +0800

    update

--- public/function.php
+++ public/function.php
@@ -1126,10 +1126,13 @@ function TagExternaToInternalLink(string $content, string $tag_name, string $htm
 		if (\Helper::options()->JPostLinkRedirect == 'on') {
 			// 使用正则表达式匹配链接并直接进行替换
 			$content = preg_replace_callback(
-				'/{' . $tag_name . '([^}]*)' . $attr_name . '\="([a-zA-z]+:\/\/[^\s]*)"([^}]*)\/}/',
-				function ($matches) use ($post_cid, $html_name, $attr_name) {
-					$redirect_link = ExternaToInternalLink($matches[2], $post_cid);
-					return '<' . $html_name . $matches[1] . $attr_name . '="' . $redirect_link . '"' . $matches[3] . '></' . $html_name . '>';
+				'/{' . $tag_name . '([^}]*)' . $attr_name . '\="(.*?)"([^}]*)\/}/',
+				function ($matches) use ($post_cid, $html_name, $attr_name, $tag_name) {
+					if (preg_match('/{' . $tag_name . '([^}]*)' . $attr_name . '\="([a-zA-z]+:\/\/[^\s]*)"([^}]*)\/}/', $matches[0])) {
+						$redirect_link = ExternaToInternalLink($matches[2], $post_cid);
+						return '<' . $html_name . $matches[1] . $attr_name . '="' . $redirect_link . '"' . $matches[3] . '></' . $html_name . '>';
+					}
+					return $matches[0];
 				},
 				$content
 			);

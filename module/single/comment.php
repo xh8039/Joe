639ee5917b897370c57712e82d183d14fedcdb3e
易航 <2136118039@qq.com>
2024-11-19 05:46:17 +0800

    update

--- module/single/comment.php
+++ module/single/comment.php
@@ -160,7 +160,7 @@ function threadedComments($comments, $options)
 {
 	$login_comment = Helper::options()->JcommentLogin == 'on' && !is_numeric(USER_ID) ? true : false;
 	if ($comments->request->getHeader('x-pjax') == 'true') {
-		echo joe\commentsAntiSpam(this()->respondId);
+		echo joe\commentsAntiSpam(this()->cid);
 	}
 ?>
 	<li class="comment-list__item">

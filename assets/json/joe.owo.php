aea467562c2762b2aa659bef96b75f10b9f19e66
易航 <2136118039@qq.com>
2024-11-21 23:25:21 +0800

    update

--- assets/json/joe.owo.php
+++ assets/json/joe.owo.php
@@ -3,7 +3,7 @@ $domain = 'blog.bri6.cn'; // 这里填写你的域名
 //判断是http还是https
 $http_type = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'ON') || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'HTTPS')) ? 'https://' : 'http://';
 //全路径
-$url = $http_type . $domain;
+$url = $http_type . trim($domain);
 header('Access-Control-Allow-Origin: ' . $url); // 允许你的域名访问
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Content-Type: application/json; charset=utf-8'); // 指定字符集为 UTF-8

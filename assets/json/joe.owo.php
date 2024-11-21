e3e66984dd667d8431f670c0893057e485c2a90c
易航 <2136118039@qq.com>
2024-11-21 23:24:56 +0800

    update

--- assets/json/joe.owo.php
+++ assets/json/joe.owo.php
@@ -1,9 +1,10 @@
 <?php
-require_once dirname(dirname(dirname(dirname(dirname(__DIR__))))) . DIRECTORY_SEPARATOR . 'config.inc.php';
-/** 初始化组件 */
-\Widget\Init::alloc();
-$host = parse_url(Helper::options()->siteUrl, PHP_URL_HOST);
-header('Access-Control-Allow-Origin: ' . $host); // 允许所有域名访问
+$domain = 'blog.bri6.cn'; // 这里填写你的域名
+//判断是http还是https
+$http_type = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'ON') || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'HTTPS')) ? 'https://' : 'http://';
+//全路径
+$url = $http_type . $domain;
+header('Access-Control-Allow-Origin: ' . $url); // 允许你的域名访问
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Content-Type: application/json; charset=utf-8'); // 指定字符集为 UTF-8
 header('Cache-Control: public, max-age=2592000');

43bc4c888e5b8c0d85a6bd2939c7c5907437be3c
易航 <2136118039@qq.com>
2024-12-21 21:36:42 +0800

    update

--- public/function.php
+++ public/function.php
@@ -558,7 +558,7 @@ function send_email($title, $subtitle, $content, $email = '')
 	$FromName = empty(\Helper::options()->JCommentMailFromName) ? \Helper::options()->title : \Helper::options()->JCommentMailFromName;
 	if (!empty(\Helper::options()->JMailApi)) {
 		$JMailApi = optionMulti(\Helper::options()->JMailApi, '||', null, ['url', 'title', 'name', 'content', 'email', 'code', '200', 'message']);
-		$send_email = \network\http\get($JMailApi['url'], [
+		$send_email = \network\http\post($JMailApi['url'], [
 			$JMailApi['title'] => $title,
 			$JMailApi['name'] => $FromName,
 			$JMailApi['content'] => $html,

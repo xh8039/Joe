8d6da7cebc7200dddebcdacb13b2a641bb231d0a
易航 <2136118039@qq.com>
2025-01-13 21:00:22 +0800

    update

--- user/head.php
+++ user/head.php
@@ -5,7 +5,6 @@ if (!defined('__TYPECHO_ROOT_DIR__')) {
 }
 $referer = empty($_GET['referer']) ? '/' : addslashes(strip_tags($_GET['referer']));
 $referer_parse = parse_url($referer);
-var_dump($referer_parse);
 $referer_host = $referer_parse['host'] ?? null;
 $referer_path = $referer_parse['path'] ?? '';
 if ($referer_host == $_SERVER['HTTP_HOST'] || substr($referer_path, 0, 1) == '/') {

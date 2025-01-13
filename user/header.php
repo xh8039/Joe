0db1fd0422e60bbf9db3e4628ce342d8f8f50c73
易航 <2136118039@qq.com>
2025-01-13 20:56:06 +0800

    update

--- user/header.php
+++ user/header.php
@@ -4,12 +4,10 @@ if (!defined('__TYPECHO_ROOT_DIR__')) {
 	exit;
 }
 $referer = empty($_GET['referer']) ? '/' : addslashes(strip_tags($_GET['referer']));
-if ($this->user->hasLogin()) {
-	$referer_parse = parse_url($referer);
-	var_dump($referer_parse);
-	$referer_host = $referer_parse['host'] ?? null;
-	$referer_path = $referer_parse['path'] ?? '';
-	if ($referer_host == $_SERVER['HTTP_HOST'] || substr($referer_path, 0, 1) == '/') {
-		echo $this->user->hasLogin() ? "<script>window.location.href='{$referer}'</script>" : "<script>window.Joe.referer='{$referer}'</script>";
-	}
+$referer_parse = parse_url($referer);
+var_dump($referer_parse);
+$referer_host = $referer_parse['host'] ?? null;
+$referer_path = $referer_parse['path'] ?? '';
+if ($referer_host == $_SERVER['HTTP_HOST'] || substr($referer_path, 0, 1) == '/') {
+	echo $this->user->hasLogin() ? "<script>window.location.href='{$referer}'</script>" : "<script>window.Joe.referer='{$referer}'</script>";
 }

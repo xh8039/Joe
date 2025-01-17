8da766837fba8e31d62d9646e25eb4a25a0d2d67
易航 <2136118039@qq.com>
2025-01-17 18:01:34 +0800

    update

--- assets/typecho/config/js/joe.config.js
+++ assets/typecho/config/js/joe.config.js
@@ -61,9 +61,7 @@ document.addEventListener("DOMContentLoaded", function () {
 			success: (data) => {
 				layer.close(loading);
 				if (data.update) {
-					layer.confirm(data.msg, {
-						btn: ['前往更新', '暂不更新']
-					}, () => {
+					layer.confirm(data.msg, { btn: data.btn }, () => {
 						openLinkInNewTab(data.download);
 					}, () => {
 						layer.alert(`<p>最怕问初衷，大梦成空。</p><p>眉间鬓上老英雄，剑甲鞮鍪封厚土，说甚擒龙。</p><p>壮志付西风，逝去无踪。</p><p>少年早作一闲翁，诗酒琴棋终日里，岁月匆匆。</p><p>不更新等着养老吗？</p>`);

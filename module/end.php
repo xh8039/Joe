5160080ba336676095ef9840f92d739b440cfc0f
易航 <2136118039@qq.com>
2025-01-19 02:45:14 +0800

    update

--- module/end.php
+++ module/end.php
@@ -10,10 +10,10 @@ $joe_action_bottom = 20;
 if ($this->options->JMusic == 'on') {
 ?>
 	<meting-js data-turbo-permanent fixed="true" preload="metadata" mutex="true" volume="0.3" autotheme="true" storage="<?= $this->options->JMusicId ?>" order="<?= $this->options->JMusicOrder ?>" server="<?= $this->options->JMusicServer ?>" type="<?= $this->options->JMusicType ?>" id="<?= $this->options->JMusicId ?>" <?= $this->options->JMusicPlay == 'on' ? 'autoplay="true"' : null ?>></meting-js>
-	<script>
+	<script data-turbo-permanent>
 		window.meting_api = `<?= empty($this->options->JMusicApi) ? '${Joe.BASE_API}/joe/api?routeType=meting&server=:server&type=:type&id=:id&r=:r' : $this->options->JMusicApi ?>`
 	</script>
-	<script src="<?= joe\theme_url('assets/plugin/Meting.js'); ?>"></script>
+	<script data-turbo-permanent src="<?= joe\theme_url('assets/plugin/Meting.js'); ?>"></script>
 <?php
 }
 ?>

8fee0d2c08ce412fc3d72f0604370a7f42f79908
易航 <2136118039@qq.com>
2025-01-20 00:26:01 +0800

    update

--- module/archive/pjax.php
+++ module/archive/pjax.php
@@ -1,6 +1,6 @@
 <?php
-if (is_string($this->request->getHeader('x-pjax-selectors'))) {
-	$selectors = json_decode($this->request->getHeader('x-pjax-selectors'));
+if (is_string($this->request->getHeader('x-ajax-selectors'))) {
+	$selectors = json_decode($this->request->getHeader('x-ajax-selectors'));
 	ob_start();
 	$this->archiveTitle(array('category' => '分类 %s 下的文章', 'search' => '包含关键字 %s 的文章', 'tag' => '标签 %s 下的文章', 'author' => '%s 发布的文章'), '', ' - ');
 	$title = ob_get_contents();

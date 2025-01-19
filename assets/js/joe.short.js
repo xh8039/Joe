3900d10a349071975ce5c52c00d20cff8d3dff1a
易航 <2136118039@qq.com>
2025-01-20 00:42:07 +0800

    update

--- assets/js/joe.short.js
+++ assets/js/joe.short.js
@@ -521,7 +521,7 @@ Joe.DOMContentLoaded.short = Joe.DOMContentLoaded.short ? Joe.DOMContentLoaded.s
 						}
 					}
 					const videoPlayer = () => {
-						$.getScript(window.Joe.CDN('dplayer/1.27.0/DPlayer.min.js'), () => {
+						let init = () => {
 							new DPlayer({
 								container: this, // 播放器容器元素
 								autoplay: this.options.autoplay, // 视频自动播放
@@ -538,10 +538,15 @@ Joe.DOMContentLoaded.short = Joe.DOMContentLoaded.short ? Joe.DOMContentLoaded.s
 									url: this.options.src
 								}
 							});
-						});
+						}
+						if (!window.DPlayer) {
+							$.getScript(window.Joe.CDN('dplayer/1.27.0/DPlayer.min.js'), init);
+						} else {
+							init();
+						}
 					}
 					if (isM3U8(this.options.src)) {
-						$.getScript(window.Joe.CDN('hls.js/1.5.13/hls.min.js'), videoPlayer);
+						window.Joe.loadJS(window.Joe.CDN('hls.js/1.5.13/hls.min.js'), videoPlayer);
 					} else {
 						videoPlayer();
 					}

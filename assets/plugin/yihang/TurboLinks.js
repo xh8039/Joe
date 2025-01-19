9b5e17adf25981e8d0525dc710c734495526532f
易航 <2136118039@qq.com>
2025-01-19 19:59:22 +0800

    update

--- assets/plugin/yihang/TurboLinks.js
+++ assets/plugin/yihang/TurboLinks.js
@@ -4,26 +4,41 @@ class TurboLinks {
 
 	pjax;
 
+	handleResponseParam = {};
+
+	/** 本次需要加载的 JavaScript文件列表 */
 	loadJSList = [];
 
-	documentScriptList = [];
+	/** 全局已经加载过的 JavaScript文件列表 */
+	static documentScriptList = [];
+
+	/** 全局已经加载过的 CSS 文件列表 */
+	static documentCSSLinkList = [];
 
 	constructor(url, options = {}) {
 		var pjax = new Pjax(options);
 		pjax._handleResponse = pjax.handleResponse;
 		pjax.handleResponse = (responseText, request, href, options) => {
-
+			this.handleResponseParam = { responseText, request, href, options };
 			const responseDocument = (new DOMParser()).parseFromString(responseText, 'text/html');
 
 			const loadStyleList = responseDocument.head.querySelectorAll('style');
 			loadStyleList.forEach(this.loadStyle);
 
+			document.querySelectorAll('link[href]').forEach(element => {
+				TurboLinks.documentCSSLinkList.push(element.href);
+			});
+			const loadCSSList = responseDocument.head.querySelectorAll('link[href]');
+			loadCSSList.forEach(this.loadCSSLink);
+
 			this.loadJSList = responseDocument.head.querySelectorAll('script');
 			if (this.loadJSList.length < 1) return pjax._handleResponse(responseText, request, href, options);
 			document.querySelectorAll('script[src]').forEach(element => {
-				this.documentScriptList.push(element.src);
+				TurboLinks.documentScriptList.push(element.src);
 			});
-			this.loadJSList.forEach(this.replaceJs);
+			this.loadJSList.forEach((element, index) => {
+				this.replaceJs(element, index);
+			});
 
 		}
 		pjax.loadUrl(url);
@@ -31,14 +46,19 @@ class TurboLinks {
 	}
 
 	JsLoaded(element, index) {
-		this.documentScriptList.push(element.src);
+		TurboLinks.documentScriptList.push(element.src);
 		if (index == (this.loadJSList.length - 1)) {
 			console.log('所有JavaScript文件都已加载！');
-			this.pjax._handleResponse(responseText, request, href, options);
+			this.pjax._handleResponse(this.handleResponseParam.responseText, this.handleResponseParam.request, this.handleResponseParam.href, this.handleResponseParam.options);
 		}
 	}
 
 	replaceJs(element, index) {
+		if (!this || !this instanceof TurboLinks) {
+			console.log(this);
+			console.error('请使用TurboLinks对象调用！');
+			return false;
+		}
 		let code = element.text || element.textContent || element.innerHTML || "";
 		let script = document.createElement("script");
 
@@ -51,21 +71,19 @@ class TurboLinks {
 		if (element.id) script.id = element.id;
 
 		if (element.src) {
-			if ($(element).attr('data-turbolinks-permanent') != undefined) {
-				if (this.documentScriptList.includes(element.src)) {
-					JsLoaded(element, index);
-					return true;
-				}
+			if ($(element).attr('data-turbolinks-permanent') != undefined && TurboLinks.documentScriptList.includes(element.src)) {
+				this.JsLoaded(element, index);
+				return true;
 			}
 			script.src = element.src;
 			script.async = false;
 			script.addEventListener('load', () => {
 				console.log('引入JS：' + element.src);
-				JsLoaded(element, index);
+				this.JsLoaded(element, index);
 			});
 			script.addEventListener('error', () => {
 				console.error('Error loading script:', element.src);
-				JsLoaded(element, index);
+				this.JsLoaded(element, index);
 			});
 			// 强制同步加载外部JS
 		}
@@ -92,23 +110,27 @@ class TurboLinks {
 
 	loadStyle(element, index) {
 		let code = element.text || element.textContent || element.innerHTML || null;
-		if (code) {
-			console.log('引入CSS：'.code);
-			let style = document.createElement('style');
-			style.type = 'text/css';
-			try {
-				style.appendChild(document.createTextNode(code));
-			} catch (ex) {
-				style.styleSheet.cssText = code; // 兼容IE
-			}
-			document.head.appendChild(style);
-		} else {
-			let link = document.createElement('link');
-			link.type = 'text/css';
-			link.rel = 'stylesheet';
-			link.href = element.href;
-			document.head.appendChild(link);
-			console.log('引入CSS：' + element.href);
+		if (!code) return false;
+		console.log('引入CSS：'.code);
+		let style = document.createElement('style');
+		style.type = 'text/css';
+		try {
+			style.appendChild(document.createTextNode(code));
+		} catch (ex) {
+			style.styleSheet.cssText = code; // 兼容IE
 		}
+		document.head.appendChild(style);
+	}
+
+	loadCSSLink(element, index) {
+		if (!element.href) return false;
+		if (TurboLinks.documentCSSLinkList.includes(element.href)) return false;
+		TurboLinks.documentCSSLinkList.push(element.href);
+		let link = document.createElement('link');
+		link.type = 'text/css';
+		link.rel = 'stylesheet';
+		link.href = element.href;
+		document.head.appendChild(link);
+		console.log('引入CSS：' + element.href);
 	}
 }
--- module/single/prism.php
+++ module/single/prism.php
@@ -8,9 +8,9 @@ if (!defined('__TYPECHO_ROOT_DIR__')) {
 <link rel="stylesheet" href="<?= joe\cdn('prism-themes/1.9.0/'  . $this->options->JPrismTheme) ?>">
 <link href="<?= joe\cdn('prism/1.9.0/plugins/line-numbers/prism-line-numbers.min.css') ?>" rel="stylesheet">
 <!-- Prism.js -->
-<script src="<?= joe\cdn('prism/1.9.0/prism.min.js') ?>"></script>
-<script src="<?= joe\cdn('prism/1.9.0/plugins/autoloader/prism-autoloader.min.js') ?>"></script>
+<script src="<?= joe\cdn('prism/1.9.0/prism.min.js') ?>" data-turbolinks-permanent></script>
+<script src="<?= joe\cdn('prism/1.9.0/plugins/autoloader/prism-autoloader.min.js') ?>" data-turbolinks-permanent></script>
 <script>
 	// Prism.plugins.autoloader.languages_path = Joe.CDN(`prism/1.9.0/components/`);
 </script>
-<script src="<?= joe\cdn('prism/1.9.0/plugins/line-numbers/prism-line-numbers.min.js') ?>"></script>
+<script src="<?= joe\cdn('prism/1.9.0/plugins/line-numbers/prism-line-numbers.min.js') ?>" data-turbolinks-permanent></script>

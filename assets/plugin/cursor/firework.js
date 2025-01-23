eeb8d64e0edeeb7be1d31adfab6f595521242478
易航 <2136118039@qq.com>
2025-01-23 22:45:07 +0800

    update

--- assets/plugin/cursor/firework.js
+++ assets/plugin/cursor/firework.js
@@ -3,7 +3,7 @@ jQuery.ajaxSetup({ cache: true });
 $.getScript(Joe.CDN_URL + 'EaselJS/1.0.2/easeljs.min.js', function () {
 	var Fireworks, GRAVITY, K, SPEED, ToRadian, canvas, context, ctx, fireBoss, repeat, stage;
 	canvas = document.createElement('canvas');
-	document.body.appendChild(canvas);
+	document.body.appendChild(document.createElement('div').appendChild(canvas));
 	context = canvas.getContext("2d");
 	canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;

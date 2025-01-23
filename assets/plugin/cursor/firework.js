f6371b9d23395a923ceaafa4d0e3a14b421febd9
易航 <2136118039@qq.com>
2025-01-23 23:20:17 +0800

    update

--- assets/plugin/cursor/firework.js
+++ assets/plugin/cursor/firework.js
@@ -36,6 +36,9 @@ var canvas = document.createElement('canvas'),
 // set canvas dimensions
 canvas.width = cw;
 canvas.height = ch;
+canvas.style.position = 'fixed';
+canvas.style.top = '0px';
+canvas.style.left = '0px';
 document.body.appendChild(canvas);
 
 // now we are going to setup our function placeholders for the entire demo

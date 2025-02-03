49696dd0d4bedbdbad36dcfdd05f573155b75174
易航 <2136118039@qq.com>
2025-02-03 09:07:52 +0800

    update

--- module/single/pjax.php
+++ module/single/pjax.php
@@ -2,9 +2,14 @@
 $selectors = $this->request->getHeader('x-ajax-selectors');
 if (is_string($selectors)) {
 	$selectors = json_decode($selectors, true);
-	if (in_array('#comment_module>.comment-list', $selectors)) $this->need('module/single/comment.php');
-	if (in_array('.joe_detail__leaving', $selectors)) $this->need('module/single/leaving.php');
-	if (!in_array('#Joe', $selectors)) exit;
+	if (in_array('#comment_module>.comment-list', $selectors)) {
+		$this->need('module/single/comment.php');
+		exit;
+	}
+	if (in_array('.joe_detail__leaving', $selectors)) {
+		$this->need('module/single/leaving.php');
+		exit;
+	}
 }
 if ($this->is('single') && strpos($this->request->getPathInfo(), '/comment-page-1') !== false) {
 	$this->response->setStatus(302);

1400e0cf940d8062db8d957f51cdb86b6dbb2c8c
易航 <2136118039@qq.com>
2025-02-08 02:05:55 +0800

    update

--- assets/js/joe.short.js
+++ assets/js/joe.short.js
@@ -579,10 +579,10 @@ Joe.DOMContentLoaded.short ||= () => {
 	if (!customElements.get('joe-code')) customElements.define('joe-code', class JoeCode extends HTMLElement {
 		constructor() {
 			super();
-			console.log(this,this.className.includes('lang-'))
+			console.log(this, this.className.includes('lang-'))
 			if (!window.Prism || !this.className.includes('lang-')) return;
 			const lang = this.className.replace('lang-', '');
-			Prism.highlight(this.innerText, Prism.languages[lang], lang);
+			console.log(Prism.highlight(this.innerText, Prism.languages[lang], lang));
 		}
 	});
 

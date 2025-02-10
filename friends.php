68ba15cfa083f03aad445940f8fb4e2a79ea4204
易航 <2136118039@qq.com>
2025-02-10 17:47:54 +0800

    update

--- friends.php
+++ friends.php
@@ -30,10 +30,11 @@ $this->need('module/single/pjax.php');
 					<?php $this->need('module/single/batten.php'); ?>
 					<?php $this->need('module/single/article.php'); ?>
 					<?php
-					$friends = [];
+					// $friends = [];
 					$friends_color = ['#F8D800', '#0396FF', '#EA5455', '#7367F0', '#32CCBC', '#F6416C', '#28C76F', '#9F44D3', '#F55555', '#736EFE', '#E96D71', '#DE4313', '#D939CD', '#4C83FF', '#F072B6', '#C346C2', '#5961F9', '#FD6585', '#465EFB', '#FFC600', '#FA742B', '#5151E5', '#BB4E75', '#FF52E5', '#49C628', '#00EAFF', '#F067B4', '#F067B4', '#ff9a9e', '#00f2fe', '#4facfe', '#f093fb', '#6fa3ef', '#bc99c4', '#46c47c', '#f9bb3c', '#e8583d', '#f68e5f'];
-					$db = Typecho\Db::get();
-					$friends = $db->fetchAll($db->select()->from('table.friends')->where('status = ?', 1)->where("FIND_IN_SET('single',position)")->order('order', Typecho\Db::SORT_DESC));
+					// $db = Typecho\Db::get();
+					$friends = think\facade\Db::name('friends')->where('status', 1)->whereRaw("FIND_IN_SET('single',position)")->order('order', 'desc')->select()->toArray();
+					// $friends = $db->fetchAll($db->select()->from('table.friends')->where('status = ?', 1)->where("FIND_IN_SET('single',position)")->order('order', Typecho\Db::SORT_DESC));
 					?>
 					<?php if (sizeof($friends) > 0 && ($this->options->JFriendsSpiderHide != 'on' || !joe\detectSpider())) : ?>
 						<style>

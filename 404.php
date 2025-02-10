ab4708d343269856dba5c68f828bd084ec2628f9
易航 <2136118039@qq.com>
2025-02-11 01:41:58 +0800

    update

--- 404.php
+++ 404.php
@@ -12,7 +12,7 @@
 		<div class="joe_container">
 			<div class="joe_main">
 				<center>
-					<div class="image-box"><img src="https://gitee.com/static/errors/images/404.png"></div>
+					<div class="image-box"><img referrerpolicy="no-referrer" rel="noreferrer" src="https://gitee.com/static/errors/images/404.png"></div>
 					<h3>你所访问的页面不存在！</h3>
 					<p>资源不存在或者没有访问权限，<a href="/">点击这里</a>返回首页</p>
 				</center>

111925a88514f152a769f4431facfcbb652faf21
易航 <2136118039@qq.com>
2025-02-11 01:41:28 +0800

    update

--- 404.php
+++ 404.php
@@ -11,13 +11,11 @@
 	<div id="Joe">
 		<div class="joe_container">
 			<div class="joe_main">
-				<div class="content">
-					<center>
-						<div class="image-box"><img src="https://gitee.com/static/errors/images/404.png"></div>
-						<h3>你所访问的页面不存在！</h3>
-						<p>资源不存在或者没有访问权限，<a href="/">点击这里</a>返回首页</p>
-					</center>
-				</div>
+				<center>
+					<div class="image-box"><img src="https://gitee.com/static/errors/images/404.png"></div>
+					<h3>你所访问的页面不存在！</h3>
+					<p>资源不存在或者没有访问权限，<a href="/">点击这里</a>返回首页</p>
+				</center>
 			</div>
 		</div>
 	</div>

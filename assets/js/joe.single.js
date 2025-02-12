9027150d424a45cdffd84f73c602a587239120f2
易航 <2136118039@qq.com>
2025-02-12 23:24:09 +0800

    update

--- assets/js/joe.single.js
+++ assets/js/joe.single.js
@@ -245,7 +245,9 @@ Joe.DOMContentLoaded.single ||= () => {
 			}
 			const item = document.querySelector('.featured-video-episode>.switch-video.active');
 			if (item.nextSibling) item.nextSibling.nextElementSibling.click();
-			// videoModule.querySelector('.dplayer-video:not(.dplayer-hide-controller)')?.classList.add('dplayer-hide-controller');
+			const classList = DPlayer.options.container.classList;
+			console.log(classList);
+			if (!classList.contains('dplayer-hide-controller')) classList.add('dplayer-hide-controller');
 		}
 		const player = new VideoPlayer(options, (DPlayer) => {
 			console.log(DPlayer);

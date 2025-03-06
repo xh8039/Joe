4151225eff6bc922a58b2ce122ab0a542d8d1c79
yh-it <2136118039@qq.com>
2025-03-06 15:53:04 +0800

    update

--- assets/js/joe.function.js
+++ assets/js/joe.function.js
@@ -204,14 +204,14 @@ window.Joe.internalForwardUrl = (string) => {
 			if ($element.attr('data-pjax-state') != undefined) return false;
 			string = string.href;
 		}
-		if (string.startsWith('/admin')) return false;
+		console.log(string);
+		if (string.startsWith('/admin') || string.startsWith(location.href + '/admin')) return false;
 		if (string.startsWith('/')) return true;
 		if (string.startsWith('#')) return false;
 		let url = new URL(string);
 		if (url.hash) return false;
 		if (url.host != location.host) return false;
 		if (url.protocol == 'javascript:') return false;
-		console.log(url);
 	} catch (error) {
 		console.log(error);
 		return false;

519e856eff51136a587ff7b13065135a67ddbbd2
yh-it <2136118039@qq.com>
2025-03-16 13:22:39 +0800

    update

--- assets/plugin/yihang/MusicPlayer.js
+++ assets/plugin/yihang/MusicPlayer.js
@@ -83,7 +83,7 @@ class MusicPlayer {
 			window.addEventListener('beforeunload', () => this.storageMusic(), { once: true });
 		}
 		if (this.OPTIONS.autoplay) {
-			['click', 'keydown', 'mousedown', 'touchstart'].forEach(event => {
+			['click', 'keydown', 'mousedown'].forEach(event => {
 				document.addEventListener(event, () => this.handleInteraction(event), { once: true });
 			});
 		}
